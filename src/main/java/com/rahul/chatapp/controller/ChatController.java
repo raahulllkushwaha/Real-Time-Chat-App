0644ba4a797ca51d9d109342ae6845c00fb6e718
Rahul Kushwaha <raahullkushwaha@gmail.com>
2026-05-22 22:35:32 +0530

    Complete the code

--- src/main/java/com/rahul/chatapp/controller/ChatController.java
+++ src/main/java/com/rahul/chatapp/controller/ChatController.java
@@ -3,15 +3,22 @@ package com.rahul.chatapp.controller;
 import com.rahul.chatapp.model.ChatMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 //@RequestMapping()
-public class ChatContrller {
-    @MessageMapping("/sendMsg")
-    @SendTo("/topic/message")
+public class ChatController {
+    @MessageMapping("/sendMessage")
+    @SendTo("/topic/messages")
     public ChatMessage sendMsg(ChatMessage message){
         return message;
+    }
+
+    @GetMapping("chat")
+    public String chat(){
+
+        return "chat";
     }
 }
--- src/main/java/com/rahul/chatapp/model/ChatMessage.java
+++ src/main/java/com/rahul/chatapp/model/ChatMessage.java
@@ -1,4 +1,14 @@
 package com.rahul.chatapp.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+//@AllArgsConstructor
 public class ChatMessage {
+    private Long id;
+    private String sender;
+    private String content;
 }
